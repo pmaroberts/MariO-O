e6432eb03d6a87f4799d016c0ae392afb1dc89d5
shop0007 <shop0007@student.monash.edu>
2022-04-17 08:10:48 +1000

    added methods to Coin class, check balance and remove coin as well as updated the constructor to fit new design. errors in Coin class due to no wallet attribute in Player class yet. Will add when Git is back up!

--- src/game/Coin.java
+++ src/game/Coin.java
@@ -1,20 +1,31 @@
 package game;
 
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.items.PickUpItemAction;
+import edu.monash.fit2099.engine.positions.GameMap;
 
-public class Coin {
-    private Player player;
+public class Coin extends Item {
+    private int value;
 
-    Coin(Player player){
-        this.player = player;
+    Coin(int value, boolean portable){
+        super("Coin $"+ value, '$', portable);
+        this.value = value;
     }
 
 
+    public boolean checkBalance(int price, Player player){
+        if (player.wallet => price) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
 
-    public boolean checkBalance(int price){
-        //if this.player.wallet => price{
-        //}
-        return true;
+    public String removeCoin(Coin coin, Actor actor, GameMap gameMap){
+        PickUpItemAction pickUpItemAction = new PickUpItemAction(coin);
+        return pickUpItemAction.execute(actor, gameMap);
     }
 }
