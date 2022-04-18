f357c72500adb938e969a3e6b17ec5bf44726cef
shop0007 <shop0007@student.monash.edu>
2022-04-19 00:44:03 +1000

    added override execute Action method, will check item type and then subtract price from wallet if there is enough balance. also added an instance of the item and the player to the class as only players can purchase but actions are designed for all actors to enact.
    ***slight issue here with unused parameters; gameMap and Actor

--- src/game/PurchaseAction.java
+++ src/game/PurchaseAction.java
@@ -2,19 +2,57 @@ package game;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
 
 public class PurchaseAction extends Action {
 
+    private final Item item;
+    private final Player player;
+
+    PurchaseAction(Item item, Player player){
+        this.item = item;
+        this.player = player;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        if (item.toString().equals("SuperMushroom")){
+            SuperMushroom superMushroom = new SuperMushroom(true);
+            if(player.getWalletBalance() >= superMushroom.getPrice()){
+                player.editBalance(-superMushroom.getPrice());
+                return superMushroom.toString() + " successfully purchased! Money remaining: $" + player.getWalletBalance();
+            }
+            else{
+                return "Not Enough Money In Wallet For Purchase!";
+            }
+        }
+        else if(item.toString().equals("PowerStar")){
+            PowerStar powerStar = new PowerStar(true);
+            if(player.getWalletBalance() >= powerStar.getPrice()){
+                player.editBalance(-powerStar.getPrice());
+                return powerStar.toString() + " successfully purchased! Money remaining: $" + player.getWalletBalance();
+            }
+            else{
+                return "Not Enough Money In Wallet For Purchase!";
+            }
 
 
+        }
+        else if(item.toString().equals("Wrench")){
+            Wrench wrench = new Wrench();
+            if(player.getWalletBalance() >= wrench.getPrice()){
+                player.editBalance(-wrench.getPrice());
+                return wrench.toString() + " successfully purchased! Money remaining: $" + player.getWalletBalance();
+            }
+            else{
+                return "Not Enough Money In Wallet For Purchase!";
+            }
+        }
+        return "error item not entered correct- debugging statement, check purchase class, line :52";
+    }
 
 
-/*
-    if (item.toString().equals("SuperMushroom")){
-        SuperMushroom superMushroom = new SuperMushroom(true);
-        Coin coin = new Coin(player);
-        coin.checkBalance(superMushroom.getPrice());
-    } */
 
     @Override
     public String menuDescription(Actor actor) {
