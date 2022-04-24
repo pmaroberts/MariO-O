4369a2c6c59709cc519312d1e0827750d4ce4d31
shop0007 <shop0007@student.monash.edu>
2022-04-25 07:37:11 +1000

    created Toad class for trading, need to implement further functionality

---
+++ src/game/actors/Toad.java
@@ -0,0 +1,17 @@
+package game.actors;
+
+import edu.monash.fit2099.engine.actors.Actor;
+
+//public class Toad extends Actor {
+
+//    Toad(){
+//        super("Toad", 'T', 999999999);
+//    }
+
+    /*
+    toad needs to be able to prompt action for actor to purchase item
+    timothy needs to add actor functionality for monologue
+    toad cannot move, or attack
+    hp is max so toad can't be killed?
+     */
+//}
