f47341b2fa3e21c316b56257626bd900a7b07672
shop0007 <shop0007@student.monash.edu>
2022-04-25 07:38:45 +1000

    added instance of PowerStar and SuperMushroom on 'the same starting ground as Mario'. created random placement of these items on left side of map using imported Utils class

--- src/game/Application.java
+++ src/game/Application.java
@@ -5,9 +5,20 @@ import java.util.List;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.items.PickUpItemAction;
 import edu.monash.fit2099.engine.positions.FancyGroundFactory;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.World;
+import game.actors.Player;
+import game.enemy.Goomba;
+import game.ground.Dirt;
+import game.ground.Floor;
+import game.ground.Wall;
+import game.magical_Items.PowerStar;
+import game.magical_Items.SuperMushroom;
+import game.ground.trees.Sprout;
+
+import java.util.Random;
 
 /**
  * The main class for the Mario World game.
@@ -47,6 +58,15 @@ public class Application {
 
 			Actor mario = new Player("Player", 'm', 100);
 			world.addPlayer(mario, gameMap.at(42, 10));
+
+			Random rand = new Random();
+			SuperMushroom superMushroom = new SuperMushroom(true);
+			//superMushroom.addSuperMushroomAction(new PickUpItemAction(superMushroom));
+			gameMap.at(rand.nextInt(39), rand.nextInt(19)).addItem(superMushroom);
+
+			PowerStar powerStar = new PowerStar(true);
+			powerStar.addPowerStarAction(new PickUpItemAction(powerStar));
+			gameMap.at(rand.nextInt(39), rand.nextInt(19)).addItem(powerStar);
 
 			// FIXME: the Goomba should be generated from the Tree
 			gameMap.at(35, 10).addActor(new Goomba());
