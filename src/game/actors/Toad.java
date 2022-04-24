c75186d9c33c8e778bd3b4b822dddeb2a840c5bf
shop0007 <shop0007@student.monash.edu>
2022-04-25 08:15:08 +1000

    Created toad actor class to just do nothing, player will check if they are in the vicinity of toad and then add purchaseaction to allowable action list

--- src/game/actors/Toad.java
+++ src/game/actors/Toad.java
@@ -1,17 +1,30 @@
 package game.actors;
 
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
 
-//public class Toad extends Actor {
+public class Toad extends Actor {
 
-//    Toad(){
-//        super("Toad", 'T', 999999999);
-//    }
+    Toad() {
+        super("Toad", 'T', 999999999);
+    }
 
-    /*
-    toad needs to be able to prompt action for actor to purchase item
-    timothy needs to add actor functionality for monologue
-    toad cannot move, or attack
-    hp is max so toad can't be killed?
-     */
-//}
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+
+        return new DoNothingAction();
+    }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = new ActionList();
+        actions.add(new DoNothingAction());
+        return actions;
+    }
+}
+
+
