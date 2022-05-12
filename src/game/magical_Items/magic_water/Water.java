5019a2791dfc1c845dac7234abef2bfe125aee06
shop0007 <shop0007@student.monash.edu>
2022-05-12 11:33:51 +1000

    created water abstract class and constructor

---
+++ src/game/magical_Items/magic_water/Water.java
@@ -0,0 +1,13 @@
+package game.magical_Items.magic_water;
+
+import game.magical_Items.ConsumableItem;
+
+
+public abstract class Water extends ConsumableItem {
+
+    public Water(){
+        super( "Water", '*',false);
+
+    }
+
+}
