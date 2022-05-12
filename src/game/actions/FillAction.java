9119625c50e7e28576553cd181191dd1325d416b
shop0007 <shop0007@student.monash.edu>
2022-05-12 14:47:43 +1000

    added FillAction class to allow mario to fill water into bottle at fountain.

---
+++ src/game/actions/FillAction.java
@@ -0,0 +1,28 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.magical_Items.magic_water.Bottle;
+import game.magical_Items.magic_water.Water;
+
+public class FillAction extends Action {
+
+    private final Bottle bottle;
+    private final Water water;
+    public FillAction(Bottle bottle, Water water){
+        this.bottle = bottle;
+        this.water = water;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        this.bottle.addToBottle(this.water);
+        return menuDescription(actor);
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + "refill" + this.water.toString();
+    }
+}
